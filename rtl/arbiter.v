35df6b619d5445d40c52c7a58e57a580cd06a129
ALLEN DONALD BOSTON <u1375766@lnissrv4.eng.utah.edu>
2022-05-05 18:32:26 -0600

    fixing syn makefile

--- rtl/arbiter.v
+++ rtl/arbiter.v
@@ -1,9 +1,9 @@
 // arbiter.v
 
 `define IDLE 2'b00
-`define GNT2 2'b01
-`define GNT1 2'b10
-`define GNT0 2'b11
+`define GNT1 2'b01
+`define GNT2 2'b10
+`define GNT3 2'b11
 
 module arbiter #(
     parameter INPUT_WIDTH   = 3,
@@ -12,11 +12,11 @@ module arbiter #(
 ) (
     input                           clk,
     input                           rstn,
-    input [INPUT_WIDTH-1:0]         r,
-    output wire [OUTPUT_WIDTH-1:0]  g
+    input [1:INPUT_WIDTH]         r,
+    output wire [1:OUTPUT_WIDTH]  g
 );
 
-    reg [Y_WIDTH-1:0] y, Y;
+    reg [Y_WIDTH:1] y, Y;
 
     // Arbiter FSM
     always @(r, Y) begin
@@ -24,45 +24,45 @@ module arbiter #(
             `IDLE: begin
                 casex (r)
                     3'b000:     Y = `IDLE;
-                    3'b1xx:     Y = `GNT2;
-                    3'b01x:     Y = `GNT1;
-                    3'b001:     Y = `GNT0; 
-                    //default:    Y = `IDLE;
+                    3'b1xx:     Y = `GNT1;
+                    3'b01x:     Y = `GNT2;
+                    3'b001:     Y = `GNT3; 
+                    default:    Y = `IDLE;
                 endcase
             end 
-            `GNT2: begin
-                if (r[2])       
-                                Y = `GNT2;
-                else            
-                                Y = `IDLE;
-            end
             `GNT1: begin
                 if (r[1])       
                                 Y = `GNT1;
                 else            
                                 Y = `IDLE;
             end
-            `GNT0: begin
-                if (r[0])       
-                                Y = `GNT0;
+            `GNT2: begin
+                if (r[2])       
+                                Y = `GNT2;
                 else            
                                 Y = `IDLE;
             end
-            //default:            Y = `IDLE;
+            `GNT3: begin
+                if (r[3])       
+                                Y = `GNT3;
+                else            
+                                Y = `IDLE;
+            end
+            default:            Y = `IDLE;
         endcase
     end
 
     // Sequential block
     always @(posedge clk ) begin
-        if (!rstn)
+        if (~rstn)
             y <= `IDLE;
         else
             y <= Y;
     end
 
     // Output
-    assign g[2] = (y == `GNT2);
     assign g[1] = (y == `GNT1);
-    assign g[0] = (y == `GNT0);
+    assign g[2] = (y == `GNT2);
+    assign g[3] = (y == `GNT3);
     
 endmodule
